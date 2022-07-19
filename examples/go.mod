5735f4d3544601b1c4e096639ab45a51c590825c
Maas Lalani <maas@lalani.dev>
2022-07-19 16:05:44 -0400

    gum demo

--- examples/go.mod
+++ examples/go.mod
@@ -3,3 +3,12 @@ module github.com/charmbracelet/frame/examples
 go 1.18
 
 replace github.com/charmbracelet/frame => ../
+
+require github.com/charmbracelet/frame v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/go-rod/rod v0.108.1 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/gson v0.7.1 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
+)
--- examples/gum/main.go
+++ examples/gum/main.go
@@ -1,6 +1,34 @@
 package main
 
-import "github.com/charmbracelet/frame"
+import (
+	"time"
+
+	"github.com/charmbracelet/frame/ffmpeg"
+	"github.com/charmbracelet/frame/keys"
+	"github.com/charmbracelet/frame/setup"
+	"github.com/go-rod/rod/lib/input"
+)
 
 func main() {
+	page, cleanup := setup.Frame(setup.Options{FramePath: "tmp/frame-%02d.png", FrameRate: 60, Width: 1200, Height: 600, Port: 7681, FontSize: 42})
+	defer cleanup()
+	defer ffmpeg.MakeGIF(ffmpeg.Options{Width: 1200, Input: "tmp/frame-%02d.png", Output: "input.gif", Framerate: 50, MaxColors: 256}).Run()
+
+	for _, kp := range keys.Type("gum input") {
+		time.Sleep(100 * time.Millisecond)
+		page.Keyboard.Type(kp)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+	page.Keyboard.Type(input.Enter)
+	time.Sleep(1 * time.Second)
+
+	for _, kp := range keys.Type("Hello, gum!") {
+		time.Sleep(100 * time.Millisecond)
+		page.Keyboard.Type(kp)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+	page.Keyboard.Type(input.Enter)
+	time.Sleep(1 * time.Second)
 }
