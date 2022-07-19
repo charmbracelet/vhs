c4f1283c6bc56315dbe224efa1e8fa1a84a98e08
Maas Lalani <maas@lalani.dev>
2022-07-19 12:37:29 -0400

    feat: better API

--- main.go
+++ main.go
@@ -1,54 +1,27 @@
 package main
 
 import (
-	"time"
-
 	"github.com/go-rod/rod/lib/input"
+	"time"
 )
 
 func main() {
 	page, cleanup := setup()
 	defer cleanup()
 
-	keypresses := []input.Key{
-		input.KeyG,
-		input.KeyU,
-		input.KeyM,
-		input.Space,
-		input.KeyI,
-		input.KeyN,
-		input.KeyP,
-		input.KeyU,
-		input.KeyT,
-		input.Space,
-		input.Minus,
-		input.Minus,
-		input.KeyW,
-		input.KeyI,
-		input.KeyD,
-		input.KeyT,
-		input.KeyH,
-		input.Space,
-		input.Digit8,
-		input.Digit0,
-		input.Enter,
-		input.KeyH,
-		input.KeyI,
-		input.Space,
-		input.KeyG,
-		input.KeyU,
-		input.KeyM,
-		shift(input.Digit1),
-		input.Enter,
-	}
-
-	for _, kp := range keypresses {
-		time.Sleep(100 * time.Millisecond)
+	for _, kp := range keys("gum input --width 80") {
+		time.Sleep(time.Millisecond * 100)
 		page.Keyboard.Type(kp)
 		page.MustWaitIdle()
-		if kp == input.Enter {
-			time.Sleep(500 * time.Millisecond)
-		}
+	}
+
+	page.Keyboard.Type(input.Enter)
+	time.Sleep(time.Second)
+
+	for _, kp := range keys("Hello, Gum!") {
+		time.Sleep(time.Millisecond * 100)
+		page.Keyboard.Type(kp)
+		page.MustWaitIdle()
 	}
 
 	time.Sleep(time.Second)
@@ -57,9 +30,4 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func shift(k input.Key) input.Key {
-	k, _ = k.Shift()
-	return k
 }
