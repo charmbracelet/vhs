529f443689dbdff6b5565b60a48e5fdb58d6683a
Maas Lalani <maas@lalani.dev>
2022-07-19 23:56:03 -0400

    feat: support non-latin characters

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -7,11 +7,16 @@ import (
 )
 
 func main() {
+	// Setup dolly with a larger font size and the output GIF as demo.gif
 	d := dolly.New(dolly.WithFontSize(42), dolly.WithOutput("demo.gif"))
+
+	// Defer cleanup which tears down all spawned processes and renders the GIF
 	defer d.Cleanup()
 
-	d.Type("echo 'Hello, Demo!'", 50*time.Millisecond)
+	// Type a command
+	d.Type("echo 'Hello, 多莉!'", 100*time.Millisecond)
 	d.Enter()
 
+	// Give some buffer time for the GIF
 	time.Sleep(time.Second)
 }
