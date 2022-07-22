7e5a25621ed68a5eb21e8bd453af6f08aab28aa6
Maas Lalani <maas@lalani.dev>
2022-07-22 16:57:19 -0400

    update spin.gif example

--- examples/gum/spin/main.go
+++ examples/gum/spin/main.go
@@ -7,10 +7,10 @@ import (
 )
 
 func main() {
-	d := dolly.New(dolly.WithOutput("spin.gif"), dolly.WithFontSize(36), dolly.WithHeight(300))
+	d := dolly.New(dolly.WithOutput("spin.gif"), dolly.WithFontSize(34), dolly.WithHeight(300))
 	defer d.Cleanup()
 
-	d.Type("gum spin --title 'Buying Gum...' -- sleep 5", dolly.WithSpeed(75))
+	d.Type("gum spin -s line --title 'Buying Gum...' sleep 5", dolly.WithSpeed(75))
 	d.Enter()
 
 	time.Sleep(5 * time.Second)
