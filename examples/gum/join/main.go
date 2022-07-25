e3986f53e58e6014d1b7e3868a82bbf94ea8c910
Maas Lalani <maas@lalani.dev>
2022-07-25 11:49:13 -0400

    fix join gif

--- examples/gum/join/main.go
+++ examples/gum/join/main.go
@@ -1,34 +1,39 @@
 package main
 
 import (
+	"os"
 	"time"
 
 	"github.com/charmbracelet/dolly"
 )
 
+const boxesFile = `border-boxes.sh`
+const boxesContent = `#!/bin/bash
+
+export I=$(gum style --padding "1 5" --border double --border-foreground 212 "I")
+export LOVE=$(gum style --padding "1 4" --border double --border-foreground 57 "LOVE")
+export BUBBLE=$(gum style --padding "1 8" --border double --border-foreground 255 "Bubble")
+export GUM=$(gum style --padding "1 5" --border double --border-foreground "#04B575" "Gum")
+`
+
 func main() {
-	d := dolly.New(dolly.WithOutput("join.gif"))
+	os.WriteFile(boxesFile, []byte(boxesContent), 777)
+	defer os.Remove(boxesFile)
+
+	d := dolly.New(dolly.WithOutput("join.gif"), dolly.WithFontSize(24))
 	defer d.Cleanup()
 
-	d.Type(`I=$(gum style I --padding "1 5" --border double --border-foreground 212)`)
+	d.Type("source ./"+boxesFile, dolly.WithSpeed(0))
 	d.Enter()
-	d.Type(`LOVE=$(gum style Love --padding "1 4" --border double --border-foreground 57)`)
-	d.Enter()
-	d.Type(`BUBBLE=$(gum style Bubble --padding "1 8" --border double --border-foreground 255)`)
-	d.Enter()
-	d.Type(`GUM=$(gum style Gum --padding "1 5" --border double --border-foreground #04B575)`)
-	d.Enter()
-
-	time.Sleep(time.Second)
-	d.Type("clear", dolly.WithSpeed(50))
-	d.Enter()
+	d.Clear()
 
 	d.Type(`TOP=$(gum join  --horizontal "$I" "$LOVE")`)
 	d.Enter()
-	time.Sleep(time.Second)
+	time.Sleep(time.Second / 2)
+
 	d.Type(`BOTTOM=$(gum join  --horizontal "$BUBBLE" "$GUM")`)
 	d.Enter()
-	time.Sleep(time.Second)
+	time.Sleep(time.Second / 2)
 
 	d.Type(`gum join --align center --vertical "$TOP" "$BOTTOM"`)
 	d.Enter()
