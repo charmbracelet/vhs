7a419241ea37dfae8b32a8b06855f0bf7c8ca990
Maas Lalani <maas@lalani.dev>
2022-08-16 17:25:20 -0400

    feat: add `dolly` command-line interface

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/charmbracelet/dolly"
 )
 
 func main() {
-	b, err := os.ReadFile("dolly.vhs")
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: dolly <file.vhs>")
+		os.Exit(1)
+	}
+
+	b, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
--- dolly.vhs
+++ dolly.vhs
@@ -1,3 +1,4 @@
 Type echo 'Hello World'
+Enter
 Backspace 5
 Sleep 1s
