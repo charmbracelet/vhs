e2e328ad7a0a1ec98b652a6f0f5c9fcc7e13463c
Maas Lalani <maas@lalani.dev>
2022-10-13 19:43:25 -0400

    feat: comment on PR

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,11 @@
 TAPE="$1"
 
 GIF="$(grep "^Output .*.gif$" $TAPE | sed 's/Output //')"
-ASCII="$(grep "^Output .*.ascii$" $TAPE | sed 's/Output //')"
 
-echo $GIF
-echo $ASCII
 
 vhs $TAPE
-cat $ASCII
+
+ASCII_OUTPUT="$(cat "$(grep "^Output .*.ascii$" $TAPE | sed 's/Output //')")"
+
+echo "::set-output name=ascii::$ASCII_OUTPUT"
+echo "::set-output name=gif::https://stuff.charm.sh/bubbletea-examples/chat.gif"
