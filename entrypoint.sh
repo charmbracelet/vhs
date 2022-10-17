e85b6e7fa38fc1f65d2ccb355d795d9cb4e72e6b
Maas Lalani <maas@lalani.dev>
2022-10-17 14:10:36 -0400

    feat: github action upload artifact

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,2 @@
 #!/bin/sh -l
-
-TAPE="$1"
-
-
-vhs $TAPE
-
-ASCII="$(cat "$(grep "^Output .*.ascii$" $TAPE | sed 's/Output //')")"
-GIF="$(base64 -w 0 "$(grep "^Output .*.gif$" $TAPE | sed 's/Output //')")"
-
-echo "::set-output name=ascii::$ASCII"
-echo "::set-output name=gif::$GIF"
+vhs "$1"
