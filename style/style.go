e99fd54a8c6a8506baa6ec55e4e7a277783c64fc
Christian Rocha <christian@rocha.is>
2022-10-26 16:12:17 -0400

    chore: clean up style definition source

--- style/style.go
+++ style/style.go
@@ -2,62 +2,28 @@ package style
 
 import "github.com/charmbracelet/lipgloss"
 
-// Background is the default background color of the theme.
-const Background = "#171717"
-
-// Foreground is the default foreground color of the theme.
-const Foreground = "#dddddd"
-
-// Black is the default black color of the theme.
-const Black = "#282a2e"
-
-// BrightBlack is the default BrightBlack color of the theme.
-const BrightBlack = "#4d4d4d"
-
-// Red is the default Red color of the theme.
-const Red = "#D74E6F"
-
-// BrightRed is the default BrightRed color of the theme.
-const BrightRed = "#FE5F86"
-
-// Green is the default Green color of the theme.
-const Green = "#31BB71"
-
-// BrightGreen is the default BrightGreen color of the theme.
-const BrightGreen = "#00D787"
-
-// Yellow is the default Yellow color of the theme.
-const Yellow = "#D3E561"
-
-// BrightYellow is the default BrightYellow color of the theme.
-const BrightYellow = "#EBFF71"
-
-// Blue is the default Blue color of the theme.
-const Blue = "#8056FF"
-
-// BrightBlue is the default BrightBlue color of the theme.
-const BrightBlue = "#9B79FF"
-
-// Magenta is the default Magenta color of the theme.
-const Magenta = "#ED61D7"
-
-// BrightMagenta is the default BrightMagenta color of the theme.
-const BrightMagenta = "#FF7AEA"
-
-// Cyan is the default Cyan color of the theme.
-const Cyan = "#04D7D7"
-
-// BrightCyan is the default BrightCyan color of the theme.
-const BrightCyan = "#00FEFE"
-
-// White is the default White color of the theme.
-const White = "#bfbfbf"
-
-// BrightWhite is the default BrightWhite color of the theme.
-const BrightWhite = "#e6e6e6"
-
-// Indigo is the default Indigo color of the theme.
-const Indigo = "#5B56E0"
+// Theme colors.
+const (
+	Background    = "#171717"
+	Foreground    = "#dddddd"
+	Black         = "#282a2e" // ansi 0
+	BrightBlack   = "#4d4d4d" // ansi 8
+	Red           = "#D74E6F" // ansi 1
+	BrightRed     = "#FE5F86" // ansi 9
+	Green         = "#31BB71" // ansi 2
+	BrightGreen   = "#00D787" // ansi 10
+	Yellow        = "#D3E561" // ansi 3
+	BrightYellow  = "#EBFF71" // ansi 11
+	Blue          = "#8056FF" // ansi 4
+	BrightBlue    = "#9B79FF" // ansi 12
+	Magenta       = "#ED61D7" // ansi 5
+	BrightMagenta = "#FF7AEA" // ansi 13
+	Cyan          = "#04D7D7" // ansi 6
+	BrightCyan    = "#00FEFE" // ansi 14
+	White         = "#bfbfbf" // ansi 7
+	BrightWhite   = "#e6e6e6" // ansi 15
+	Indigo        = "#5B56E0"
+)
 
 const defaultWidth = 80
 
