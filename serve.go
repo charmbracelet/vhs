f8c1fc070ce208646e93b7140610924eeefc96af
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-10-27 21:37:00 -0300

    chore: fmt
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- serve.go
+++ serve.go
@@ -33,104 +33,102 @@ type config struct {
 	UID  int    `env:"VHS_UID" envDefault:"0"`
 }
 
-var (
-	serveCmd = &cobra.Command{
-		Use:   "serve",
-		Short: "Start the VHS SSH server",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var cfg config
-			if err := env.Parse(&cfg); err != nil {
-				return err
-			}
-			key := cfg.Key
-			if key == "" {
-				key = filepath.Join(".ssh", "vhs_ed25519")
-			}
-			s, err := wish.NewServer(
-				wish.WithAddress(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)),
-				wish.WithHostKeyPath(key),
-				wish.WithMiddleware(
-					func(h ssh.Handler) ssh.Handler {
-						return func(s ssh.Session) {
-							// Request for vhs must be passed in through stdin, which
-							// implies that there is no PTY.
-							//
-							// In the future, we should support PTY by providing a
-							// Bubble Tea interface for VHS.
-							//
-							// Ideally, users can SSH into the server and get a
-							// walk through of how to write a .tape file.
-							_, _, isPty := s.Pty()
-							if isPty {
-								wish.Println(s, "PTY is not supported")
-								_ = s.Exit(1)
-								return
-							}
-
-							// Read stdin passed from the client.
-							// This is the .tape file which contains the VHS commands.
-							//
-							// ssh vhs.charm.sh < demo.tape
-							var b bytes.Buffer
-							_, err := io.Copy(&b, s)
-							if err != nil {
-								wish.Errorln(s, err)
-								_ = s.Exit(1)
-								return
-							}
-
-							//nolint:gosec
-							rand := rand.Int63n(maxNumber)
-							tempFile := filepath.Join(os.TempDir(), fmt.Sprintf("vhs-%d.gif", rand))
-
-							err = Evaluate(b.String(), s.Stderr(), func(v *VHS) {
-								v.Options.Video.Output.GIF = tempFile
-								// Disable generating MP4 & WebM.
-								v.Options.Video.Output.MP4 = ""
-								v.Options.Video.Output.WebM = ""
-							})
-							if err != nil {
-								_ = s.Exit(1)
-							}
-
-							gif, _ := os.ReadFile(tempFile)
-							wish.Print(s, string(gif))
-							_ = os.Remove(tempFile)
-
-							h(s)
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start the VHS SSH server",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var cfg config
+		if err := env.Parse(&cfg); err != nil {
+			return err
+		}
+		key := cfg.Key
+		if key == "" {
+			key = filepath.Join(".ssh", "vhs_ed25519")
+		}
+		s, err := wish.NewServer(
+			wish.WithAddress(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)),
+			wish.WithHostKeyPath(key),
+			wish.WithMiddleware(
+				func(h ssh.Handler) ssh.Handler {
+					return func(s ssh.Session) {
+						// Request for vhs must be passed in through stdin, which
+						// implies that there is no PTY.
+						//
+						// In the future, we should support PTY by providing a
+						// Bubble Tea interface for VHS.
+						//
+						// Ideally, users can SSH into the server and get a
+						// walk through of how to write a .tape file.
+						_, _, isPty := s.Pty()
+						if isPty {
+							wish.Println(s, "PTY is not supported")
+							_ = s.Exit(1)
+							return
 						}
-					},
-					logging.Middleware(),
-				),
-			)
-			if err != nil {
-				log.Fatalln(err)
-			}
 
-			done := make(chan os.Signal, 1)
-			signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
-			log.Printf("Starting SSH server on %s:%d", cfg.Host, cfg.Port)
-			go func() {
-				gid, uid := cfg.GID, cfg.UID
-				if gid != 0 && uid != 0 {
-					log.Printf("Starting server with GID: %d, UID: %d", gid, uid)
-					if err := dropUserPrivileges(gid, uid); err != nil {
-						log.Fatalln(err)
+						// Read stdin passed from the client.
+						// This is the .tape file which contains the VHS commands.
+						//
+						// ssh vhs.charm.sh < demo.tape
+						var b bytes.Buffer
+						_, err := io.Copy(&b, s)
+						if err != nil {
+							wish.Errorln(s, err)
+							_ = s.Exit(1)
+							return
+						}
+
+						//nolint:gosec
+						rand := rand.Int63n(maxNumber)
+						tempFile := filepath.Join(os.TempDir(), fmt.Sprintf("vhs-%d.gif", rand))
+
+						err = Evaluate(b.String(), s.Stderr(), func(v *VHS) {
+							v.Options.Video.Output.GIF = tempFile
+							// Disable generating MP4 & WebM.
+							v.Options.Video.Output.MP4 = ""
+							v.Options.Video.Output.WebM = ""
+						})
+						if err != nil {
+							_ = s.Exit(1)
+						}
+
+						gif, _ := os.ReadFile(tempFile)
+						wish.Print(s, string(gif))
+						_ = os.Remove(tempFile)
+
+						h(s)
 					}
-				}
-				if err = s.ListenAndServe(); err != nil {
+				},
+				logging.Middleware(),
+			),
+		)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		done := make(chan os.Signal, 1)
+		signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+		log.Printf("Starting SSH server on %s:%d", cfg.Host, cfg.Port)
+		go func() {
+			gid, uid := cfg.GID, cfg.UID
+			if gid != 0 && uid != 0 {
+				log.Printf("Starting server with GID: %d, UID: %d", gid, uid)
+				if err := dropUserPrivileges(gid, uid); err != nil {
 					log.Fatalln(err)
 				}
-			}()
-
-			<-done
-			log.Println("Stopping SSH server")
-			ctx, cancel := context.WithTimeout(context.Background(), timeout)
-			defer func() { cancel() }()
-			if err := s.Shutdown(ctx); err != nil {
+			}
+			if err = s.ListenAndServe(); err != nil {
 				log.Fatalln(err)
 			}
-			return nil
-		},
-	}
-)
+		}()
+
+		<-done
+		log.Println("Stopping SSH server")
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer func() { cancel() }()
+		if err := s.Shutdown(ctx); err != nil {
+			log.Fatalln(err)
+		}
+		return nil
+	},
+}
