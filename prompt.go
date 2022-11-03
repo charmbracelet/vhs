652bada884e56a9c2a3f3630401b57ed6d681174
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-11-03 10:27:30 -0300

    fix: cmd

--- prompt.go
+++ prompt.go
@@ -2,11 +2,11 @@ package main
 
 // Supported shells of VHS
 const (
-	BASH       = "bash"
-	CMD        = "cmd"
-	FISH       = "fish"
-	POWERSHELL = "pwsh"
-	ZSH        = "zsh"
+	bash       = "bash"
+	cmdexe     = "cmd"
+	fish       = "fish"
+	powershell = "pwsh"
+	zsh        = "zsh"
 )
 
 // Shell is a type that contains a prompt and the command to set up the shell.
@@ -17,24 +17,24 @@ type Shell struct {
 
 // Shells contains a mapping from shell names to their Shell struct.
 var Shells = map[string]Shell{
-	BASH: {
+	bash: {
 		Prompt:  "\\[\\e[38;2;90;86;224m\\]> \\[\\e[0m\\]",
 		Command: ` set +o history; unset PROMPT_COMMAND; export PS1="%s"; clear;`,
 	},
-	ZSH: {
+	zsh: {
 		Prompt:  `%F{#5B56E0}> %F{reset_color}`,
 		Command: ` clear; zsh --login --histnostore; unsetopt PROMPT_SP; unset PROMPT; export PS1="%s"; clear`,
 	},
-	FISH: {
+	fish: {
 		Prompt:  `function fish_prompt; echo -e "$(set_color 5B56E0)> $(set_color normal)"; end`,
 		Command: `clear; fish --login --private -C 'function fish_greeting; end' -C '%s'`,
 	},
-	POWERSHELL: {
+	powershell: {
 		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
 		Command: ` clear; pwsh -Login -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
 	},
-	CMD: {
-		Prompt:  "> ",
-		Command: ` clear; export PS1="%s"; clear`,
+	cmdexe: {
+		Prompt:  "$g",
+		Command: ` cls && set prompt=%s && cls`,
 	},
 }
