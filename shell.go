charmbracelet/vhs

commit b0c663c9bc8065d73412a442d8672f4fe59cffa0

fix(pwsh): add powershell shell
pwsh is only added on newer versions of PowerShell which might not be
pre-installed in Windows 10

shell.go
@@ -5,7 +5,8 @@ const (
 	bash       = "bash"
 	cmdexe     = "cmd"
 	fish       = "fish"
-	powershell = "pwsh"
+	powershell = "powershell"
+	pwsh       = "pwsh"
 	zsh        = "zsh"
 )
 
@@ -30,6 +31,10 @@ var Shells = map[string]Shell{
 		Command: `clear; fish --login --private -C 'function fish_greeting; end' -C '%s'`,
 	},
 	powershell: {
+		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
+		Command: ` clear; powershell -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
+	},
+	pwsh: {
 		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
 		Command: ` clear; pwsh -Login -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
 	},

tty_windows.go
@@ -3,15 +3,23 @@
 
 package main
 
-import "golang.org/x/sys/windows"
+import (
+	"os/exec"
 
-const defaultShell = powershell
+	"golang.org/x/sys/windows"
+)
+
+var defaultShell = cmdexe
 
 func defaultShellWithArgs() []string {
 	major, _, _ := windows.RtlGetNtVersionNumbers()
 	if major >= 10 {
-		return []string{"powershell"}
+		if _, err := exec.LookPath("pwsh"); err == nil {
+			defaultShell = pwsh
+		} else {
+			defaultShell = powershell
+		}
 	}
 
-	return []string{"cmd"}
+	return []string{defaultShell}
 }

commit a5bba5b3fe55a7073668ae12a6674fa0ac02cc95

fix: powershell prompt
Properly escape powershell/pwsh prompts

Fixes: b0c663c9bc80 ("fix(pwsh): add powershell shell")

shell.go
@@ -31,11 +31,11 @@ var Shells = map[string]Shell{
 		Command: `clear; fish --login --private -C 'function fish_greeting; end' -C '%s'`,
 	},
 	powershell: {
-		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
+		Prompt:  "Function prompt {Write-Host \\\"> \\\" -ForegroundColor Blue -NoNewLine; return \\\"`0\\\" }",
 		Command: ` clear; powershell -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
 	},
 	pwsh: {
-		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
+		Prompt:  "Function prompt {Write-Host \\\"> \\\" -ForegroundColor Blue -NoNewLine; return \\\"`0\\\" }",
 		Command: ` clear; pwsh -Login -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
 	},
 	cmdexe: {