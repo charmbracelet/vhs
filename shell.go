charmbracelet/vhs

commit b0c663c9bc8065d73412a442d8672f4fe59cffa0

fix(pwsh): add powershell shell
pwsh is only added on newer versions of PowerShell which might not be
pre-installed in Windows 10

shell.go
@@ -5,7 +5,8 @@ const (
 	bash       = "bash"
 	cmdexe     = "cmd"
 	fish       = "fish"
-	powershell = "pwsh"
+	powershell = "powershell"
+	pwsh       = "pwsh"
 	zsh        = "zsh"
 )
 
@@ -30,6 +31,10 @@ var Shells = map[string]Shell{
 		Command: `clear; fish --login --private -C 'function fish_greeting; end' -C '%s'`,
 	},
 	powershell: {
+		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
+		Command: ` clear; powershell -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
+	},
+	pwsh: {
 		Prompt:  "Function prompt {Write-Host \"> \" -ForegroundColor Blue -NoNewLine; return \"`0\" }",
 		Command: ` clear; pwsh -Login -NoLogo -NoExit -Command 'Set-PSReadLineOption -HistorySaveStyle SaveNothing; %s'`,
 	},

tty_windows.go
@@ -3,15 +3,23 @@
 
 package main
 
-import "golang.org/x/sys/windows"
+import (
+	"os/exec"
 
-const defaultShell = powershell
+	"golang.org/x/sys/windows"
+)
+
+var defaultShell = cmdexe
 
 func defaultShellWithArgs() []string {
 	major, _, _ := windows.RtlGetNtVersionNumbers()
 	if major >= 10 {
-		return []string{"powershell"}
+		if _, err := exec.LookPath("pwsh"); err == nil {
+			defaultShell = pwsh
+		} else {
+			defaultShell = powershell
+		}
 	}
 
-	return []string{"cmd"}
+	return []string{defaultShell}
 }

commit 12b5f4b9df9b3d5fbe8cd4d6df0680d74fd585e0

fix: wait for prompt to be ready
Ensure that the prompt is set as expected before proceeding

Fixes: https://github.com/charmbracelet/vhs/pull/146
Fixes: b0c663c9bc80 ("fix(pwsh): add powershell shell")

shell.go
@@ -10,6 +10,9 @@ const (
 	zsh        = "zsh"
 )
 
+// The default uncolored prompt
+const promptASCII = "> "
+
 // Shell is a type that contains a prompt and the command to set up the shell.
 type Shell struct {
 	Prompt  string

vhs.go
@@ -131,6 +131,12 @@ func (vhs *VHS) Setup() {
 		MustInput(shellCommand).
 		MustType(input.Enter)
 
+	// Wait for prompt to be ready
+	vhs.Page = vhs.Page.MustWait(`(prompt) => {
+		term.selectAll()
+		return term.getSelection().split('\n')?.[0]?.startsWith(prompt)
+	}`, promptASCII)
+
 	// Apply options to the terminal
 	// By this point the setting commands have been executed, so the `opts` struct is up to date.
 	vhs.Page.MustEval(fmt.Sprintf("() => { term.options = { fontSize: %d, fontFamily: '%s', letterSpacing: %f, lineHeight: %f, theme: %s } }",