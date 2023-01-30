cf32ba774d97bf864910c62549aef962b4114e8b
Carlos A Becker <caarlos0@users.noreply.github.com>
2023-01-30 14:56:51 -0300

    fix: improving bash, fish, zsh a bit
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- shell.go
+++ shell.go
@@ -20,15 +20,15 @@ type Shell struct {
 var Shells = map[string]Shell{
 	bash: {
 		Prompt:  "\\[\\e[38;2;90;86;224m\\]> \\[\\e[0m\\]",
-		Command: ` clear; PS1="%s" bash --login --norc --noprofile; set +o history; clear;`,
+		Command: ` PS1="%s" bash --login --norc --noprofile +o history; clear;`,
 	},
 	zsh: {
 		Prompt:  `%F{#5B56E0}> %F{reset_color}`,
-		Command: ` clear; PROMPT="%s" zsh --login --histnostore --no-rcs; clear`,
+		Command: ` PROMPT="%s" zsh --login --histnostore --no-rcs; clear;`,
 	},
 	fish: {
 		Prompt:  `function fish_prompt; echo -e "$(set_color 5B56E0)> $(set_color normal)"; end`,
-		Command: ` clear; fish --login --no-config --private -C 'function fish_greeting; end' -C '%s'`,
+		Command: ` fish --login --no-config --private -C 'function fish_greeting; end' -C '%s'; clear;`,
 	},
 	powershell: {
 		Prompt:  `Set-PSReadLineOption -HistorySaveStyle SaveNothing; Function prompt { Write-Host -ForegroundColor Blue -NoNewLine '>'; return ' ' }`,
