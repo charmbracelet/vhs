5b5cafb18e5996dec280b9c7b640bd4926b6b662
may h <mhoque95@gmail.com>
2024-06-16 22:37:22 -0400

    Add various tests for keystroke.go

--- keystroke_test.go
+++ keystroke_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/go-rod/rod/lib/input"
 )
 
 // checkKeyStrokeEvents checks if the key stroke events are as expected.
@@ -15,7 +17,83 @@ func checkKeyStrokeEvents(t *testing.T, events *KeyStrokeEvents, expected ...str
 
 func defaultKeyStrokeEvents() *KeyStrokeEvents {
 	events := NewKeyStrokeEvents(DefaultMaxDisplaySize)
-	events.enabled = true
+	events.Enable()
 
 	return events
+}
+
+func TestKeyStrokeEventsRemembersKeyStrokes(t *testing.T) {
+	events := defaultKeyStrokeEvents()
+	events.Push("a")
+	events.Push("b")
+	events.Push("c")
+	checkKeyStrokeEvents(t, events, "a", "ab", "abc")
+}
+
+func TestKeyStrokeEventsHonorsMaxDisplaySize(t *testing.T) {
+	events := defaultKeyStrokeEvents()
+	events.maxDisplaySize = 2
+
+	events.Push("a")
+	events.Push("b")
+	events.Push("c")
+
+	// NOTE: It should not be "ab", but "bc" at the end -- we should be acting
+	// like a ring buffer.
+	checkKeyStrokeEvents(t, events, "a", "ab", "bc")
+}
+
+func TestKeyStrokeEventsShowsNothingIfDisabled(t *testing.T) {
+	events := defaultKeyStrokeEvents()
+	events.Disable()
+
+	events.Push("a")
+	events.Push("b")
+	events.Push("c")
+
+	checkKeyStrokeEvents(t, events)
+}
+
+func TestKeyStrokeEventsKeyToDisplay(t *testing.T) {
+	cases := []struct {
+		name     string
+		key      input.Key
+		expected string
+	}{
+		{
+			name:     "letter",
+			key:      input.KeyA,
+			expected: "a",
+		},
+		{
+			name:     "number",
+			key:      input.Digit1,
+			expected: "1",
+		},
+		{
+			name:     "symbol no override",
+			key:      input.Minus,
+			expected: "-",
+		},
+		{
+			name:     "shifted key",
+			key:      shift(input.Minus),
+			expected: "_",
+		},
+		{
+			name:     "symbol override",
+			key:      input.Backspace,
+			expected: "⌫",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			if actual := keyToDisplay(tc.key); tc.expected != actual {
+				t.Fatalf("expected display %q, got %q", tc.expected, actual)
+			}
+		})
+	}
 }
