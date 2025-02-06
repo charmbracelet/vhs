21856751f3ebd94fd390e2623565e1695942fd1a
Raphael Amorim <rapha850@gmail.com>
2025-02-06 11:45:26 -0300

    support additional alt/ctrl/shift special characters

--- token/token.go
+++ token/token.go
@@ -17,13 +17,19 @@ type Token struct {
 
 // Tokens for the VHS language
 const (
-	AT      = "@"
-	EQUAL   = "="
-	PLUS    = "+"
-	PERCENT = "%"
-	SLASH   = "/"
-	DOT     = "."
-	DASH    = "-"
+	AT        = "@"
+	EQUAL     = "="
+	PLUS      = "+"
+	PERCENT   = "%"
+	SLASH     = "/"
+	BACKSLASH = "\\"
+	DOT       = "."
+	DASH      = "-"
+
+	MINUS         = "-"
+	RIGHT_BRACKET = "]"
+	LEFT_BRACKET  = "["
+	CARET         = "^"
 
 	EM           = "EM"
 	MILLISECONDS = "MILLISECONDS"
