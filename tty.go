a910222e2b0b63d102cbdad4410193c4190e41e4
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2026-08-02 19:12:17 +0530

    fix(ttyd): explicitly define the cwd during ttyd startup to bypass invalid path bug on windows

--- tty.go
+++ tty.go
@@ -14,6 +14,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"runtime"
 )
 
 // randomPort returns a random port number that is not in use.
@@ -34,6 +35,10 @@ func buildTtyCmd(port int, shell Shell) *exec.Cmd {
 		"-t", "customGlyphs=true",
 		"--once", // will allow one connection and exit
 		"--writable",
+	}
+
+	if runtime.GOOS == "windows" {
+		args = append(args, "-w", ".")
 	}
 
 	args = append(args, shell.Command...)
